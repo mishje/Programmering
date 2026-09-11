5df54575bc2fa1b78375956cc1a61ff976fd3c31
Mishje <121200475+mishje@users.noreply.github.com>
2026-09-11 13:58:02 +0200

    2-4 på riktigt

---
+++ Uppgift2-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du låna bilen?");
+            int dag = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt i km planerar du att köra bilen?");
+            int dist = int.Parse(Console.ReadLine());
+            int total = 300 + dist + 500 * (dag - 1);
+            Console.WriteLine($"Du kommer att få betala totalt {total}kr vid lån av denna bil.");
+            Console.ReadKey();
+        }
+    }
+}
